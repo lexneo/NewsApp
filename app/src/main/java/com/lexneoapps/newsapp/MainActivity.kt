b2ea4cfd26d6fa57c2722c78e5dfef7f9327e376
lexneo <82415841+lexneo@users.noreply.github.com>
2022-02-15 02:38:03 +0100

    added layouts,fragments and navigation

--- app/src/main/java/com/lexneoapps/newsapp/MainActivity.kt
+++ app/src/main/java/com/lexneoapps/newsapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.lexneoapps.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.lexneoapps.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/lexneoapps/newsapp/ui/SavedNewsFragment.kt
@@ -0,0 +1,42 @@
+package com.lexneoapps.newsapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lexneoapps.newsapp.R
+import com.lexneoapps.newsapp.databinding.FragmentsSavedNewsBinding
+
+class SavedNewsFragment : Fragment(R.layout.fragments_saved_news) {
+
+
+    private var _binding: FragmentsSavedNewsBinding? = null
+
+
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentsSavedNewsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
