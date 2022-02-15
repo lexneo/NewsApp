f59bfb3b80d4e70f168361b69e20dc29c4d267ac
lexneo <82415841+lexneo@users.noreply.github.com>
2022-02-15 13:22:02 +0100

    retrofit setup

---
+++ app/src/main/java/com/lexneoapps/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.lexneoapps.newsapp.models
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/lexneoapps/newsapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.lexneoapps.newsapp.utils
+
+object Constants {
+
+    const val API_KEY = "1b27da20138448fdae30b8c4aa6cbf7a"
+    const val BASE_URL = "https://newsapi.org"
+}
